d9c63afbe69e53ab959432f51cc73518f27b8c4b
Alona Pehasheva <aleesikaleesik@gmail.com>
2024-05-21 15:30:37 +0300

    add tasks js and html

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,13 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = Number.parseFloat(content);
-  const paddingValue = Number.parseFloat(padding);
-  const borderThickness = Number.parseFloat(border);
-  const result = contentWidth + (paddingValue + borderThickness) * 2;
-  return result;
-}
+const nameInput = document.querySelector('#name-input');
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', () => {
+  const trimmedValue = nameInput.value.trim();
+
+  if (trimmedValue) {
+    nameOutput.textContent = trimmedValue;
+  } else {
+    nameOutput.textContent = 'Anonymous';
+  }
+});
